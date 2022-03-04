89ee558fd25a348fa011fc98ee9bd872cc108b49
Gabriel Anjos <gabriel.anjos@businessintegrator.com.br>
2022-03-04 15:56:22 -0300

    teste inicial de conexao com o banco

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express' // node não comprede import 
-
+import express from "express"; // node não comprede import
+import "reflect-metadata";
 const app = express();
+
+import "./database";
 
 
 // criar servidor
-app.listen(3333, () => console.log("Servidor em operação!"))
+app.listen(3333, () => console.log("Servidor em operação!"));
